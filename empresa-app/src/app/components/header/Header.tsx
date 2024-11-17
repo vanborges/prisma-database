6d07768b50b515449a03bb4973c229bc0fa3f35b
Fabio Ramos <fabiowilliam466@gmail.com>
2024-11-17 19:35:53 -0400

    17/11 corrigido osBO

--- empresa-app/src/app/components/header/Header.tsx
+++ empresa-app/src/app/components/header/Header.tsx
@@ -21,12 +21,20 @@ const Header: React.FC<HeaderProps> = ({ contas, onSelectConta }) => {
   return (
     <header style={styles.header}>
       <h1>Controle Financeiro</h1>
+<<<<<<< HEAD
       <div style={styles.selectContainer}>
+=======
+      <div style={{ width: "300px" }}>
+>>>>>>> 71ae85b (17/11 corrigido osBO)
         <Select
           options={options}
           onChange={handleChange}
           placeholder="Selecione uma Conta"
           styles={customStyles}
+<<<<<<< HEAD
+=======
+          isClearable
+>>>>>>> 71ae85b (17/11 corrigido osBO)
         />
       </div>
     </header>
@@ -42,9 +50,27 @@ const styles = {
     backgroundColor: "#2c6e49",
     color: "#fff",
   },
+<<<<<<< HEAD
   selectContainer: {
     width: "300px", // Ajuste o tamanho do select
   },
+=======
+};
+
+const customStyles = {
+  control: (base: any) => ({
+    ...base,
+    borderColor: "#ddd",
+    borderRadius: "5px",
+    padding: "5px",
+    fontSize: "16px",
+  }),
+  option: (base: any, { isFocused }: { isFocused: boolean }) => ({
+    ...base,
+    backgroundColor: isFocused ? "#d5f5e3" : "#fff",
+    color: "#000",
+  }),
+>>>>>>> 71ae85b (17/11 corrigido osBO)
 };
 
 // Estilos personalizados para o react-select
